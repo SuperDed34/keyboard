3d20398772435c386dc4f66f14ffef1a7425447a
SuperDed34 <maxnozord@gmail.com>
2023-04-30 18:03:53 +0100

    feat: realized rendering js and starter CSS layout

--- assets/js/render.js
+++ assets/js/render.js
@@ -1,5 +1,4 @@
 let body = document.querySelector('body');
-console.log(body);
 
 document.body.onload = addElement;
 
@@ -17,82 +16,60 @@ function addElement() {
   textPole.classList.add("header__textarea");
 
 
-  let KeyboardWrapper = document.createElement("div");
-  KeyboardWrapper.classList.add("keyboard");
+  // Находим элемент, куда будем добавлять клавиатуру
+  let keyboardContainer = document.createElement("keyboard");
 
+  // Создаем клавиатуру
+  let keyboardLayout = [
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\', 'Del'],
+    ['CapsLock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'Enter'],
+    ['Shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', '\&#9650', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '\&#9668', '\&#9660', '\&#9658', 'Ctrl']
+  ];
 
-  let firstRow = document.createElement("div");
-  firstRow.classList.add("keyboard__keys");
-
-  let secondRow = document.createElement("div");
-  secondRow.classList.add("keyboard__keys");
-
-  let thridRow = document.createElement("div");
-  thridRow.classList.add("keyboard_keys");
-
-  let fourRow = document.createElement("div");
-  fourRow.classList.add("keyboard_keys");
+  let rusKeyboardLayout = [
+    ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'Backspace'],
+    ['Tab', 'й', 'ц', 'у', 'к', 'е', 'н', 'г', 'ш', 'щ', 'з', 'х', 'ъ', '\\', 'Del'],
+    ['CapsLock', 'ф', 'ы', 'в', 'а', 'п', 'р', 'о', 'л', 'д', 'ж', 'э', 'Enter'],
+    ['Shift', 'я', 'ч', 'с', 'м', 'и', 'т', 'ь', 'б', 'ю', '.', '\&#9650', 'Shift'],
+    ['Ctrl', 'Win', 'Alt', 'Space', 'Alt', '\&#9668', '\&#9660', '\&#9658', 'Ctrl']
+  ];
   
-  let fiveRow = document.createElement("div");
-  fiveRow.classList.add("keyboard_keys");
+  // Создаем элементы клавиатуры и добавляем их в контейнер
+  for (let i = 0; i < keyboardLayout.length; i++) {
+    let row = document.createElement('div');
+    row.classList.add('keyboard-row');
 
-  let keyboardFrames = document.querySelectorAll(".keyboard__keys");
-
-  function addKeys(row) {
-    let counter = 0;
-    let rows = 0;
-    if (row === firstRow) {
-      counter = 13;
-      rows = 0;
-    } else if (row === secondRow) {
-      counter = 14;
-      rows = 1;
-    } else if (row === thridRow) {
-      counter = 12;
-      rows = 2;
-    } else if (row === fourRow) {
-      counter = 12;
-      rows = 3;
-    } else if (row === fiveRow) {
-      counter = 8;
-      rows = 4;
+    for (let j = 0; j < keyboardLayout[i].length; j++) {
+      var key = document.createElement('div');
+      key.classList.add('keyboard-key');
+      key.innerHTML = keyboardLayout[i][j];
+      if (key.textContent === 'Backspace' || key.textContent === 'CapsLock' || key.textContent === 'Shift') {
+        key.style.width = '100px';
+      } else if (key.textContent === 'Tab') {
+        key.style.width = '44px';
+      } else if (key.textContent === 'Enter') {
+        key.style.width = '96px';
+      } else if (key.textContent === 'Space') {
+        key.style.width = '380px';
+      }
+      row.appendChild(key);
     }
 
-
-    for (let i = 0; i < counter ; i++){
-      let newKey = document.createElement("div");
-      newKey.classList.add("keyboard__key");
-      console.log(counter);
-      console.log(keyboardFrames);
-
-      let keyLayer = keyboardFrames[rows];
-      console.log(keyLayer);
-      keyLayer.appendChild(newKey);
-    }
+    keyboardContainer.appendChild(row);
   }
-
-  addKeys(firstRow);
-  addKeys(secondRow);
-  addKeys(thridRow);
-  addKeys(fourRow);
-  addKeys(fiveRow);
+  
+  
 
   let footer = document.createElement("div");
   footer.classList.add("footer");
   footer.textContent = "Клавиатура создана в операционной системе Windows Для переключения языка комбинация: левые ctrl+alt";
 
-
   body.appendChild(header);
   header.appendChild(headerH1);
   header.appendChild(textPole);
-
-  body.appendChild(KeyboardWrapper);
-  KeyboardWrapper.appendChild(firstRow);
-  KeyboardWrapper.appendChild(secondRow);
-  KeyboardWrapper.appendChild(thridRow);
-  KeyboardWrapper.appendChild(fourRow);
-  KeyboardWrapper.appendChild(fiveRow);
-
+  body.appendChild(keyboardContainer);
   body.appendChild(footer);
 
 }
